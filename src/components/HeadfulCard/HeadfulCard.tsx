a81644ae54cd968c4e8b003b148d9f6943c3fa6d
Robin Holzinger <robin.holzinger@tum.de>
2023-10-06 23:48:13 +0200

    Improve performance via next/Image

--- src/components/HeadfulCard/HeadfulCard.tsx
+++ src/components/HeadfulCard/HeadfulCard.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 /**
  * Supports links in markdown format
  */
@@ -13,14 +15,23 @@ export default function HeadfulCard({
   return (
     <div className="relative sm:pt-12 sm:px-16 sm:max-w-2xl sm:mx-auto">
       <div className="relative">
-        <img src={bgImagePath} alt="Header background image" className="w-full h-auto z-0 sm:rounded-t-[64px]" />
+        <Image
+          src={bgImagePath}
+          alt="Header background image"
+          className="w-full h-auto z-0 sm:rounded-t-[64px]"
+          width={512}
+          height={256}
+          // sizes="(max-width: 512px) 100px"
+        />
         <div className="absolute inset-0 bg-black opacity-5 sm:rounded-t-[64px]"></div>
       </div>
       <div className="transform -translate-y-24">
         <div className="absolute z-20 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
-          <img
+          <Image
             src={profileImagePath}
             alt="Profile Picture"
+            width={256}
+            height={256}
             className="w-32 h-32 xs:w-36 xs:h-36 sm:w-40 sm:h-40 rounded-full border-8 border-white"
           />
         </div>
